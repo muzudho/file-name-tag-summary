e2e383913339c21f5e80399fd981e82d7ee2baaa
むずでょ <muzudho1@gmail.com>
2022-04-23 18:33:51 +0900

    tag split

--- file_name_tag_summary.py
+++ file_name_tag_summary.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import re
 
 
 def input_change_current_directory(prompt_message):
@@ -24,16 +25,56 @@ Files
     return files
 
 
+def input_y(prompt_message):
+    """はい？"""
+    print(prompt_message)
+
+    answer = input()
+
+    return answer.upper() == "Y"
+
+
 def main():
 
-    input_change_current_directory("""Which directory?
+    # ディレクトリーを選んでください
+    while True:
+
+        input_change_current_directory("""Which directory?
 Example: .
 """)
 
-    print(f"""Current directory: {os.getcwd()}""")
+        print(f"""Current directory: {os.getcwd()}""")
 
-    # フィル名を一覧します
-    files = list_files_flat()
+        # フィル名を一覧します
+        files = list_files_flat()
+
+        is_y = input_y("""
+Are you sure this is the right directory (y/n)?
+""")
+
+        if is_y:
+            break
+        else:
+            print("Canceld")
+
+    # 集計を始めます
+    pattern = re.compile("^.*__(.*)__.*$")
+
+    for file in files:
+        result = pattern.match(file)
+        if result:
+            # Matched
+            tag_section = result.group(1)
+            print(f"tag_section={tag_section}")
+
+            tags = tag_section.split("_")
+            for tag in tags:
+                print(f"tag={tag}")
+
+        else:
+            pass
+
+        pass
 
 
 main()
