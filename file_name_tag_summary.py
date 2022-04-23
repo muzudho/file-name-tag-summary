c2d3fbc3388bf9826a7e802a683c44e452cf6e32
むずでょ <muzudho1@gmail.com>
2022-04-23 19:21:38 +0900

    summary

--- file_name_tag_summary.py
+++ file_name_tag_summary.py
@@ -69,14 +69,27 @@ Are you sure this is the right directory (y/n)?
             tag_section = result.group(1)
             print(f"tag_section={tag_section}")
 
-            tags = tag_section.split("_")
-            for tag in tags:
-                print(f"tag={tag}")
+            # tags = tag_section.split("_")
+            # for tag in tags:
+            #     print(f"tag={tag}")
 
-            if tag_section in summary:
-                summary[tag_section] += 1
-            else:
-                summary[tag_section] = 1
+            while True:
+                if tag_section in summary:
+                    print(f"+=1")
+                    summary[tag_section] += 1
+                else:
+                    print(f"=1")
+                    summary[tag_section] = 1
+
+                # _ が付いていれば、最後の１つを外したい
+                if "_" in tag_section:
+                    print(f"before={tag_section}")
+                    tag_section = tag_section.rsplit("_", 1)[0]
+                    print(f"after={tag_section}")
+
+                else:
+                    print(f"break {tag_section}")
+                    break
 
         else:
             pass
